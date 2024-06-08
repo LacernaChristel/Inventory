65c3365d08461e42adc0c1a434c83affeba1de96
LacernaChristel <127415481+LacernaChristel@users.noreply.github.com>
2024-06-08 20:34:25 +0800

    Update dataForm.jsx

--- Inventory-main/component/dataForm.jsx
+++ Inventory-main/component/dataForm.jsx
@@ -4,7 +4,7 @@ import "./dataForm.css";
 import Navbar from "./navbar";
 import jsPDF from "jspdf";
 
-const API_URL = "https://willowy-entremet-de7643.netlify.app/.netlify/functions/api/";
+const API_URL = "https://steady-marshmallow-8d4ae8.netlify.app/.netlify/functions/api/":
 
 function DataForm() {
   const [inventory, setInventory] = useState([]);
